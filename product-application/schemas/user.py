47704d38c076797ca43f35363e8255093d51adf8
KshitijBahul <chunks.3@gmail.com>
2020-04-14 16:50:58 +0530

    Added a gitignore and Accept only and reject only fields

--- product-application/schemas/user.py
+++ product-application/schemas/user.py
@@ -1,6 +1,9 @@
 from marshmallow import Schema, fields
 
 class UserSchema(Schema):
+    class Meta:
+        load_only = ("password",) # telling to use this field only for loading and not returing 
+        dump_only = ("id",) # telling to use this field only for returing and not accepting
     id = fields.Int()
     username = fields.Str(required=True)
     password = fields.Str(required=True)
